49d1c02bdedfdf56eecc3769e2e098cc79eb3acf
Nathan Chere <git@nathanchere.com.au>
2016-10-07 13:04:06 +0200

    CCI DS 01 left rotation

--- crackingthecodinginterview/datastructures/01_left_rotation.exs
+++ crackingthecodinginterview/datastructures/01_left_rotation.exs
@@ -1,7 +1,26 @@
 # HackerRank "Cracking The Coding Interview" - Data Structures (Arrays) - Left Rotation
 
 defmodule Solution do
+  defp read_array() do
+    IO.gets("")
+    |> String.strip
+    |> String.split(" ")
+    |> Enum.map(fn(x) -> String.to_integer(x) end)
+  end
+
+  defp left_shift(input, times) when times == 0, do: input
+
+  defp left_shift([head|tail], times) do
+    left_shift(tail ++ [head], times - 1)
+  end
+
   def main() do
+  [_, iterations] = read_array
+
+  read_array
+  |> left_shift(iterations)
+  |> Enum.join(" ")
+  |> IO.puts
 
   end
 end
